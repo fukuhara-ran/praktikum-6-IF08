399499663dd24f01bcd34290370193661111b723
Reksa Panduasa <panduasareksa@students.amikom.ac.id>
2023-04-13 14:43:14 +0700

    color mode

--- quiz/index.js
+++ quiz/index.js
@@ -1,4 +1,6 @@
 let display = document.getElementById('display');
+const button = document.getElementById('mode');
+const html = document.querySelector('html');
 
 function append(value) {
     display.value += value;
@@ -18,4 +20,34 @@ function operate(operator) {
 
 function calculate() {
     display.value = eval(display.value);
+}
+
+button.addEventListener('click', function () {
+    if(html.dataset.colorMode === 'dark'){
+        html.dataset.colorMode = 'light';
+        this.textContent = 'Light Mode';
+    }
+    else{
+        html.dataset.colorMode = 'dark';
+        this.textContent = 'Dark Mode';
+    }
+  })
+
+function ValidateInput (value) {
+	let last_input = input.slice(-1);
+	let operators = ["+", "-", "*", "/"];
+
+	if (value == "." && last_input == ".") {
+		return false;
+	}
+
+	if (operators.includes(value)) {
+		if (operators.includes(last_input)) {
+			return false;
+		} else {
+			return true;
+		}
+	}
+
+	return true;
 }
